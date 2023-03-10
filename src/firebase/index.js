d23ac3d43f62dddf3d7cd24d75bae4a2d0659332
Bruno <bruno.grobelsek@hotmail.com>
2023-03-10 14:22:42 +0100

    .env added and working!

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-_nnzHlcX8gAeEILoIYmqpzA8XAoO14Y",
-  authDomain: "fir-test-f4ff3.firebaseapp.com",
-  projectId: "fir-test-f4ff3",
-  storageBucket: "fir-test-f4ff3.appspot.com",
-  messagingSenderId: "282031538513",
-  appId: "1:282031538513:web:ec1cc929ae1849b4609370"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MSID,
+  appId: process.env.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
