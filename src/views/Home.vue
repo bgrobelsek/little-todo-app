981f44a43b28387496a5afa9d29901ed50f13070
8string <bruno.grobelsek@hotmail.com>
2023-08-16 12:49:09 +0200

    some changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,23 +31,6 @@
           :to='item.to'
           link
         >
-        <div>
-    <v-toolbar
-      dark
-      prominent
-      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Vuetify</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-export</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </div>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
